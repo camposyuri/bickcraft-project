368c882e8b79b8412aef8d9f7e9f6fb522a11c00
yuri_humann <yuri@humann.com.br>
2019-09-25 14:02:22 -0300

    page-home em construção - Section portfolio concluida

--- source/php/page-home.php
+++ source/php/page-home.php
@@ -23,36 +23,42 @@
         <div class="row">
             <div class="col-md-4 pt-3">
                 <div class="card">
-                    <img class="card-img-top img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/passeio.png"
+                    <img class="card-img-top img-fluid"
+                         src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/passeio.png"
                          alt="Imagem de capa do card">
                     <div class="card-body">
                         <h6 class="title-card-products text-uppercase text-center">Passeio</h6>
                         <hr class="hr-col">
-                        <p class="card-text pt-3">Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
+                        <p class="card-text pt-3">Ainda assim, existem dúvidas a respeito de como a necessidade de
+                            renovação</p>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-4 pt-3">
                 <div class="card">
-                    <img class="card-img-top img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/esporte.png"
+                    <img class="card-img-top img-fluid"
+                         src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/esporte.png"
                          alt="Imagem de capa do card">
                     <div class="card-body">
                         <h6 class="title-card-products text-uppercase text-center">Esporte</h6>
                         <hr class="hr-col">
-                        <p class="card-text pt-3">Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
+                        <p class="card-text pt-3">Ainda assim, existem dúvidas a respeito de como a necessidade de
+                            renovação</p>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-4 pt-3">
                 <div class="card">
-                    <img class="card-img-top img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/retro.png"
+                    <img class="card-img-top img-fluid"
+                         src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/retro.png"
                          alt="Imagem de capa do card">
                     <div class="card-body">
                         <h6 class="title-card-products text-uppercase text-center">Retro</h6>
                         <hr class="hr-col">
-                        <p class="card-text pt-3">Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
+                        <p class="card-text pt-3">Ainda assim, existem dúvidas a respeito de como a necessidade de
+                            renovação</p>
                     </div>
                 </div>
             </div>
@@ -61,6 +67,30 @@
             <div class="col-md-12 d-flex justify-content-center">
                 <a href="#" class="btn btn-products text-uppercase">Produtos</a>
             </div>
+        </div>
+    </div>
+</section>
+
+<section class="section-home-portfolio bg-black py-5">
+    <div class="container">
+        <h1 class="text-center text-uppercase title-home-portfolio">Portfólio</h1>
+        <hr class="hr-col mb-4">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <img class="img-fluid pb-2" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/retro.jpg" alt="">
+            </div>
+
+            <div class="col-md-5">
+                <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/passeio.jpg" alt="">
+            </div>
+        </div>
+        <div class="col-12 d-flex justify-content-center pt-4">
+            <img class="img-fluid d-none d-md-block" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/esporte.jpg" alt="">
+        </div>
+
+        <p class="text-center m-auto pt-4 paragraph-portfolio">conheça mais o nosso portfólio</p>
+        <div class="col-md-12 d-flex justify-content-center">
+            <a href="#" class="btn btn-portfolio text-uppercase mt-3">Portfólio</a>
         </div>
     </div>
 </section>
