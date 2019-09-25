d3bd80c9bfc84bfb71af519b48653c0c74e3159a
yuri_humann <yuri@humann.com.br>
2019-09-25 09:59:57 -0300

    page-home em construção - parallax effect mouse moviment

--- source/php/page-home.php
+++ source/php/page-home.php
@@ -2,10 +2,12 @@
 
 
 <section class="section-home banner-home">
-    <div class="container text-center py-md-5">
+    <div class="container text-center py-3 py-md-5">
         <h1 class="text-white pt-5 title-home text-uppercase">Bicicletas feitas a mãos</h1>
         <div class="col-md-6 m-auto py-3">
+            <hr class="hr-col">
             <span class="text-white span-format">“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</span>
+            <hr class="hr-col">
         </div>
         <p class="text-white text-uppercase pt-3">William Morris</p>
         <div class="col-md-12 d-flex justify-content-center">
@@ -16,7 +18,8 @@
 
 <section class="section-home-products py-5">
     <div class="container pt-3">
-        <h1 class="text-center text-uppercase title-home-products pb-5">Produtos</h1>
+        <h1 class="text-center text-uppercase title-home-products">Produtos</h1>
+        <hr class="hr-col">
         <div class="row">
             <div class="col-md-4 pt-3">
                 <div class="card">
@@ -24,7 +27,8 @@
                          alt="Imagem de capa do card">
                     <div class="card-body">
                         <h6 class="title-card-products text-uppercase text-center">Passeio</h6>
-                        <p class="card-text">Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
+                        <hr class="hr-col">
+                        <p class="card-text pt-3">Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
                     </div>
                 </div>
             </div>
@@ -35,7 +39,8 @@
                          alt="Imagem de capa do card">
                     <div class="card-body">
                         <h6 class="title-card-products text-uppercase text-center">Esporte</h6>
-                        <p class="card-text">Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
+                        <hr class="hr-col">
+                        <p class="card-text pt-3">Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
                     </div>
                 </div>
             </div>
@@ -46,12 +51,13 @@
                          alt="Imagem de capa do card">
                     <div class="card-body">
                         <h6 class="title-card-products text-uppercase text-center">Retro</h6>
-                        <p class="card-text">Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
+                        <hr class="hr-col">
+                        <p class="card-text pt-3">Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
                     </div>
                 </div>
             </div>
 
-            <p class="text-center m-auto py-4">clique aqui e veja os detalhes dos produtos</p>
+            <p class="text-center m-auto py-3 paragraph-text">clique aqui e veja os detalhes dos produtos</p>
             <div class="col-md-12 d-flex justify-content-center">
                 <a href="#" class="btn btn-products text-uppercase">Produtos</a>
             </div>
